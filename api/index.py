86298cfa5c03fbd4819e8283582d29fb70b19e02
monosniper <vanteateam@gmail.com>
2022-08-04 02:39:23 +0500

    tes

--- api/index.py
+++ api/index.py
@@ -1,10 +1,17 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, Response
 from flask_sslify import SSLify
 
 BOT_TOKEN = '5581884599:AAFgpkbtGEbtfCSvj0w1L2DDL75TjSDIRnw'
 
 app = Flask(__name__)
 ssslify = SSLify(app)
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % path, mimetype="text/html")
+
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -13,6 +20,7 @@ def webhook():
         return 'success', 200
     else:
         abort(400)
+
 
 if __name__ == '__main__':
     app.run()
